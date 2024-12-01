5134150e1fa7f9e8c340436a9d1d61d4b9f5a6fb
Gustav R. Jansen <gustav.r.jansen@gmail.com>
2024-12-01 11:37:09 -0500

    Advent day 1a reads the an input file.

--- advent_2024_day_1a/src/main.rs
+++ advent_2024_day_1a/src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+
+    println!("Reading file: {}", config.file_path);
+
+    let content = fs::read_to_string(config.file_path)
+        .expect("Can't read the file");
+
+    println!("Read:\n{content}");
+}
+
+struct Config {
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 2 {
+            panic!("Need filename as first argument.");
+        }
+
+        let file_path = args[1].clone();
+
+        Config { file_path }
+    }
 }
